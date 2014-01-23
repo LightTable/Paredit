b89154b1a98eb178f05dc56a52c566e07c5af88b
Chris Granger <ibdknox@gmail.com>
2014-01-23 11:01:56 -0800

    0.0.2
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+##Changes
+
+###0.0.2
+* ADDED: `Paredit: Unwrap parent` command
--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject com.lighttable/paredit "0.0.1"
+(defproject com.lighttable/paredit "0.0.2"
   :description "paredit plugin for Light Table"
   :dependencies [[org.clojure/clojure "1.5.1"]])
