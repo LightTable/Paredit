ca55fb9eb521bdf24e15965313eb0b7803f40737
Chris Granger <ibdknox@gmail.com>
2014-01-23 22:47:46 -0800

    add source map, 0.0.3
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject com.lighttable/paredit "0.0.2"
+(defproject com.lighttable/paredit "0.0.3"
   :description "paredit plugin for Light Table"
   :dependencies [[org.clojure/clojure "1.5.1"]])
